9e528d32229225e79d5ed981f31cd679435da00d
halufun <frogillius@hotmail.com>
2025-02-08 22:18:44 -0800

    moved directories

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 setlocal
 
 REM Get the directory where the batch script is located
-cd /d "%~dp0"
+cd app
 
 REM Check if Caddy exists
 if not exist "caddy.exe" (
@@ -13,13 +13,7 @@ if not exist "caddy.exe" (
 
 REM Start Caddy on port 6969
 echo Starting Caddy server on http://localhost:6969 ...
-start "" "%CD%\caddy.exe" file-server --listen :6969 --browse
+start "" "caddy.exe" file-server --listen :6969 --browse
 start http://localhost:6969/game
 
-REM Keep the window open until user presses a key
-echo Caddy is running. Press any key to stop.
-pause
-
-REM Stop Caddy when the user presses a key
-taskkill /IM caddy.exe /F
 exit
